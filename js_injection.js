5a51ee6bf0ea5b0193ebf80a8fde038f092e0a1b
Thin Ba Shane (Luna) <thinbashane@gmail.com>
2018-02-07 01:00:00 +0630

    Add files via upload

--- js_injection.js
+++ js_injection.js
@@ -5,5 +5,5 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 app.get('/:id', function(request, response) {
 	console.log('Input -> '+request.params.id);
-  response.send('user ' + request.params.id);
+  response.send('user ' + eval(request.params.id));
 });
